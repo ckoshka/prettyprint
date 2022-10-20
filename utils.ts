f25b53338ff89c690d488b78c773682008ac6c19
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-10-21 08:55:36 +1300

    ability to control mix strength

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 import { chroma } from "./deps.ts";
 
-export const themeColor = (theme: chroma.Scale) =>
+export const themeColor = (theme: chroma.Scale, mixStrength = 0.65) =>
 	(colorname: string) => {
 		const color = chroma(colorname);
-		return color.mix(theme(color.luminance()), 0.65, "lch");
+		return color.mix(theme(color.luminance()), mixStrength, "lch");
 	};
